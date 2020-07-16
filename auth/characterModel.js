35cdbd56384e93bc65a27f443bdc7bd72aa62622
Zac Smith <zrs3141592@gmail.com>
2020-07-16 16:16:29 -0600

    scaffold model

---
+++ auth/characterModel.js
@@ -0,0 +1,22 @@
+const db = require('../database/dbConfig.js')
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove,
+}
+
+function add() {}
+
+function find() {}
+
+function findBy() {}
+
+function findById() {}
+
+function update() {}
+
+function remove() {}
--- auth/characterRouter.js
+++ auth/characterRouter.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
+const Characters = require('./characterModel.js')
 
 // const express = require('express')
 // const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => {})
+
+router.get('/test', (req, res) => {
   res.send('yes its working')
 })
 
